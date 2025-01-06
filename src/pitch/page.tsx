af67cab117cda2af16950070893bfa0d45f142d4
Troy <troy.megaride@gmail.com>
2025-01-06 15:52:02 +0800

    added pitch route

--- src/pitch/page.tsx
+++ src/pitch/page.tsx
@@ -42,7 +42,7 @@ const PitchPage: FC = () => {
     <div className="bg-black">
       {/* Progress indicator */}
       <motion.div 
-        className="fixed top-0 left-0 right-0 h-1 bg-yellow-400 origin-left z-50"
+        className="fixed top-0 left-0 right-0 h-1 bg-yellow-410 origin-left z-50"
         style={{ scaleX: scrollYProgress }}
       />
       
@@ -108,3 +108,4 @@ const PitchPage: FC = () => {
 };
 
 export default PitchPage;
+//cmment
