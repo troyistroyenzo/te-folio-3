f73e32fe015f2b83984fe1f65cb32a68f9d13880
Troy <yortozne@gmail.com>
2025-01-06 16:06:49 +0800

    Added Router Dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 // src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Routes, Route } from 'react-router-dom'
 import App from './App'
 import PitchPage from './pitch/page'
 import './index.css'
+import { HashRouter as Router } from 'react-router-dom'  // Change this line
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
